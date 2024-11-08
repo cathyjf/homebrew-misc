83c6f02b2105d4a2c756aae0f935a82840ffbdbe
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2024-11-08 15:51:23 -0800

    keychain-interpose 0.1.2 (#5)

--- Casks/keychain-interpose.rb
+++ Casks/keychain-interpose.rb
@@ -1,8 +1,8 @@
 cask "keychain-interpose" do
-  version "0.1.1"
-  sha256 "56abb42d2acbc69635fbab0352fd2badbf6e40954d857d03d2eb35208d3efd48"
+  version "0.1.2"
+  sha256 "149ef266a4cf093d3271cb5367fcb72b57215420c544d17f0c942d4ce266df30"
 
-  url "https://github.com/cathyjf/keychain-interpose/releases/download/v#{version}/keychain-interpose-#{version}.zip"
+  url "https://github.com/cathyjf/keychain-interpose/releases/download/v#{version}/keychain-interpose-v#{version}.zip"
   name "keychain-interpose"
   desc "Store GPG secret key files in the keychain"
   homepage "https://github.com/cathyjf/keychain-interpose"
