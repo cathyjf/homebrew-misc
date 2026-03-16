a47ed8dc6137478e980978cf398a894d5962ac1e
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2026-03-16 11:04:23 -0700

    sshfs 3.7.5

--- Casks/sshfs.rb
+++ Casks/sshfs.rb
@@ -1,17 +1,16 @@
 cask "sshfs" do
-  version "2.5.0"
-  sha256 "f8f4f71814273ea42dbe6cd92199f7cff418571ffd1b10c0608878d3472d2162"
+  version "3.7.5"
+  sha256 "611713612179cf7ccd2995051165da7d19e0ca199ae70d9680c3d3551f456d46"
 
-  url "https://github.com/osxfuse/sshfs/releases/download/osxfuse-sshfs-#{version}/sshfs-#{version}.pkg",
-      verified: "github.com/osxfuse/sshfs/"
+  url "https://github.com/libfuse/sshfs/releases/download/sshfs-#{version}/sshfs-#{version}.pkg",
+      verified: "github.com/libfuse/sshfs/"
   name "sshfs"
   desc "File system client based on SSH File Transfer Protocol"
   homepage "https://osxfuse.github.io/"
 
-  conflicts_with formula: ["homebrew/core/sshfs", "homebrew/core/sshfs-mac", "gromgit/fuse/sshfs-mac"]
   depends_on cask: "macfuse"
 
   pkg "sshfs-#{version}.pkg"
 
-  uninstall pkgutil: "com.github.osxfuse.pkg.SSHFS"
+  uninstall pkgutil: "io.macfuse.installer.components.sshfs"
 end
